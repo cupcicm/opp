ab9d867b6c791395352b436f711f03352e8a2f01
Manu Cupcic <cupcicm@gmail.com>
2023-08-26 12:23:33 +0200

    Adds a few misc utilities.

---
+++ git/config.go
@@ -0,0 +1,41 @@
+package git
+
+import (
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+func GetGithubToken() string {
+	return viper.GetString("github.token")
+}
+
+func GetGithubUsername() string {
+	return viper.GetString("github.login")
+}
+
+func GetGithubRepo() string {
+	return viper.GetString("repo.github")
+}
+
+// The first part of the repo, before the slash.
+func GetGithubOwner() string {
+	repo := viper.GetString("repo.github")
+	slash := strings.LastIndex(repo, "/")
+	return repo[:slash]
+}
+
+// The second part of the repo, after the slash.
+func GetGithubRepoName() string {
+	repo := viper.GetString("repo.github")
+	slash := strings.LastIndex(repo, "/")
+	return repo[slash+1:]
+}
+
+func GetRemoteName() string {
+	return viper.GetString("repo.remote")
+}
+
+func GetBaseBranch() string {
+	return viper.GetString("repo.branch")
+}
