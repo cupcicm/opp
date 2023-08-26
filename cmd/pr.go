9ab93e7fe828a406c1e7aa0655ebf3b1201800c9
Manu Cupcic <cupcicm@gmail.com>
2023-08-26 14:29:46 +0200

    Adds a command to create a PR.
    It pushes to a new branch, creates the github pull requests.
    It knows about dependent pull requests.

---
+++ cmd/pr.go
@@ -0,0 +1,173 @@
+package cmd
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/cupcicm/opp/git"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/google/go-github/github"
+	"github.com/spf13/cobra"
+)
+
+var ErrLostPCreationRaceCondition error = errors.New("lost race condition when creating PR")
+var ErrLostPrCreationRaceConditionMultipleTimes error = errors.New("lost race condition when creating PR too many times, aborting")
+
+func PrCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "pr",
+		Aliases: []string{"pull-request", "new"},
+		Args:    cobra.MaximumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var repo = git.Current()
+			var headCommit plumbing.Hash
+			if len(args) == 0 {
+				var head = git.Must(repo.Head())
+				if !head.Name().IsBranch() {
+					return errors.New("works only when on a branch")
+				}
+				headCommit = head.Hash()
+			} else {
+				hash, err := repo.ResolveRevision(plumbing.Revision(args[0]))
+				if err != nil {
+					return err
+				}
+				headCommit = *hash
+			}
+			ancestor, commits, err := repo.FindBranchingPoint(headCommit)
+			if err != nil {
+				return err
+			}
+			tip := git.Must(repo.GetLocalTip(ancestor))
+			if ancestor.IsPr() && tip.Hash == headCommit {
+				fmt.Println("You are on a branch that has already been pushed as a PR")
+				fmt.Println("Use opp up to update that PR instead.")
+				//return nil
+			}
+			// Create a new PR then.
+			pr := createPr{Repo: repo, Github: git.NewClient(cmd.Context())}
+			return pr.Create(cmd.Context(), headCommit, commits, ancestor)
+		},
+	}
+
+	return cmd
+}
+
+type createPr struct {
+	Repo   *git.Repo
+	Github *github.Client
+}
+
+func RemoteBranch(branch string) string {
+	return fmt.Sprintf("%s/%s", git.GetGithubUsername(), branch)
+}
+
+func (c *createPr) Create(ctx context.Context, hash plumbing.Hash, commits []*object.Commit, ancestor git.Branch) error {
+
+	title, body := c.GetBodyAndTitle(commits)
+
+	pr, err := c.create(ctx, hash, ancestor, title, body)
+	if err != nil {
+		return err
+	}
+	c.createLocalBranchForPr(*pr.Number, hash, ancestor)
+	localPr := git.NewLocalPr(c.Repo, *pr.Number)
+	localPr.SetAncestor(ancestor)
+	c.Repo.SetTrackingBranch(localPr, ancestor)
+	fmt.Printf("https://github.com/%s/pull/%d\n", git.GetGithubRepo(), *pr.Number)
+	return err
+}
+
+func (c *createPr) GetBodyAndTitle(commits []*object.Commit) (string, string) {
+	sort.Slice(commits, func(i, j int) bool {
+		return len(commits[i].Message) > len(commits[j].Message)
+	})
+	title, body, _ := strings.Cut(commits[0].Message, "\n")
+	return strings.TrimSpace(title), strings.TrimSpace(body)
+}
+
+func (c *createPr) createLocalBranchForPr(number int, hash plumbing.Hash, ancestor git.Branch) {
+	c.Repo.CreateBranch(&config.Branch{
+		Name:   git.LocalBranchForPr(number),
+		Remote: git.RemoteBranchForPr(number),
+		Merge:  plumbing.NewBranchReferenceName(ancestor.RemoteName()),
+		Rebase: "true",
+	})
+
+	ref := plumbing.NewBranchReferenceName(git.LocalBranchForPr(number))
+	c.Repo.Storer.SetReference(plumbing.NewHashReference(ref, hash))
+}
+
+func (c *createPr) create(ctx context.Context, hash plumbing.Hash, ancestor git.Branch, title string, body string) (*github.PullRequest, error) {
+	for attempts := 0; attempts < 3; attempts++ {
+		pr, err := c.createOnce(ctx, hash, ancestor, title, body)
+		if err == nil {
+			return pr, nil
+		}
+		if err != ErrLostPCreationRaceCondition {
+			return nil, err
+		}
+	}
+	return nil, ErrLostPrCreationRaceConditionMultipleTimes
+}
+
+func (c *createPr) createOnce(ctx context.Context, hash plumbing.Hash, ancestor git.Branch, title string, body string) (*github.PullRequest, error) {
+	lastPr, err := c.getLastPrNumber(ctx)
+	if err != nil {
+		return nil, err
+	}
+	remote := git.RemoteBranchForPr(lastPr + 1)
+	base := ancestor.RemoteName()
+	err = c.Repo.Push(ctx, hash, remote)
+	if err != nil {
+		return nil, err
+	}
+	pull := github.NewPullRequest{
+		Title: &title,
+		Head:  &remote,
+		Base:  &base,
+		Body:  &body,
+	}
+	pr, _, err := c.Github.PullRequests.Create(
+		ctx,
+		git.GetGithubUsername(),
+		git.GetGithubRepoName(),
+		&pull,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if *pr.Number != lastPr+1 {
+		return nil, ErrLostPCreationRaceCondition
+	}
+	return pr, nil
+}
+
+func (c *createPr) getLastPrNumber(ctx context.Context) (int, error) {
+	pr, _, err := c.Github.PullRequests.List(
+		ctx,
+		git.GetGithubOwner(),
+		git.GetGithubRepoName(),
+		&github.PullRequestListOptions{
+			State:     "all",
+			Sort:      "created",
+			Direction: "desc",
+			ListOptions: github.ListOptions{
+				Page:    0,
+				PerPage: 1,
+			},
+		},
+	)
+	if err != nil {
+		return 0, err
+	}
+	if len(pr) == 0 {
+		return 0, nil
+	}
+	return *pr[0].Number, nil
+}
---
+++ git/branch.go
@@ -0,0 +1,188 @@
+package git
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+
+	"github.com/go-git/go-git/v5/plumbing"
+	"gopkg.in/yaml.v3"
+)
+
+type LocalPr struct {
+	PrNumber int
+	HasState bool
+	state    *state
+	Repo     *Repo
+}
+
+type branch struct {
+	Repo  *Repo
+	Name  string
+	Local bool
+}
+
+type Branch interface {
+	IsPr() bool
+	LocalName() string
+	RemoteName() string
+}
+
+type state struct {
+	Ancestor struct {
+		Name string
+	}
+}
+
+func (b *LocalPr) StateFile() string {
+	return path.Join(b.Repo.DotOpDir(), "state", b.LocalBranch())
+}
+
+func (b *LocalPr) saveState() error {
+	content, err := yaml.Marshal(b.state)
+	if err != nil {
+		return err
+	}
+	os.MkdirAll(path.Dir(b.StateFile()), 0755)
+	return os.WriteFile(b.StateFile(), content, 0644)
+}
+
+func (b *LocalPr) loadState() {
+	hasState := FileExists(b.StateFile())
+	if hasState {
+		b.state = Must(loadState(b.StateFile()))
+	} else {
+		b.state = &state{}
+	}
+	b.HasState = true
+}
+func loadState(file string) (*state, error) {
+	content, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	state := state{}
+	err = yaml.Unmarshal(content, &state)
+	if err != nil {
+		return nil, err
+	}
+	return &state, nil
+}
+
+func (b *LocalPr) DeleteState() {
+	os.Remove(b.StateFile())
+}
+
+func NewLocalPr(repo *Repo, prNumber int) *LocalPr {
+	pr := LocalPr{
+		Repo:     repo,
+		PrNumber: prNumber,
+	}
+	pr.loadState()
+	return &pr
+}
+
+func NewBranch(repo *Repo, name string) Branch {
+	branch := branch{
+		Repo: repo,
+		Name: name,
+	}
+	return &branch
+}
+
+func (b *LocalPr) IsPr() bool {
+	return true
+}
+
+func (b *LocalPr) LocalName() string {
+	return b.LocalBranch()
+}
+
+func (b *LocalPr) RemoteName() string {
+	return b.RemoteBranch()
+}
+
+func (b *branch) IsPr() bool {
+	return false
+}
+
+func (b *branch) LocalName() string {
+	return b.Name
+}
+
+func (b *branch) RemoteName() string {
+	return b.Name
+}
+
+func (b *LocalPr) GetAncestor() (Branch, error) {
+	if b.state.Ancestor.Name == "" {
+		return nil, errors.New("no ancestors have been set")
+	}
+	number, errNotAPr := ExtractPrNumber(b.state.Ancestor.Name)
+	if errNotAPr != nil {
+		return NewBranch(b.Repo, b.state.Ancestor.Name), nil
+	}
+	pr := NewLocalPr(b.Repo, number)
+	_, err := b.Repo.GetLocalTip(pr)
+	if err == plumbing.ErrReferenceNotFound {
+		// This PR has been merged / deleted locally.
+		ancestor, err := pr.GetAncestor()
+		if err != nil {
+			return nil, err
+		}
+		b.SetAncestor(ancestor)
+		b.Repo.CleanupAfterMerge(pr)
+		return ancestor, nil
+	}
+	return pr, nil
+}
+
+func (b *LocalPr) SetAncestor(branch Branch) {
+	b.state.Ancestor.Name = branch.LocalName()
+	b.saveState()
+}
+
+func (b *LocalPr) LocalBranch() string {
+	return LocalBranchForPr(b.PrNumber)
+}
+
+func (b *LocalPr) RemoteBranch() string {
+	return RemoteBranchForPr(b.PrNumber)
+}
+
+func RemoteBranchForPr(number int) string {
+	return fmt.Sprintf("%s/pr/%d", GetGithubUsername(), number)
+}
+
+func LocalBranchForPr(number int) string {
+	return fmt.Sprintf("pr/%d", number)
+}
+
+func (b *LocalPr) Push(ctx context.Context) error {
+	tip, err := b.Repo.GetLocalTip(b)
+	if err != nil {
+		return fmt.Errorf("PR %s has no local branch", b.LocalBranch())
+	}
+	return b.Repo.Push(ctx, tip.Hash, b.RemoteBranch())
+}
+
+var ErrNotAPrBranch = errors.New("not a pr branch")
+
+func ExtractPrNumber(branchname string) (int, error) {
+	parts := strings.Split(branchname, "/")
+	if len(parts) != 2 {
+		return 0, ErrNotAPrBranch
+	}
+	if parts[0] != "pr" {
+		return 0, ErrNotAPrBranch
+	}
+	number, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return 0, ErrNotAPrBranch
+	}
+	return number, nil
+}
