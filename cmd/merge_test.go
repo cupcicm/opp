3a977cf66a072d233611591ab82b8faaec5806cc
Manu Cupcic <cupcicm@gmail.com>
2023-09-03 21:39:47 +0200

    Makes opp merge not merge dependent PRs.

--- cmd/merge_test.go
+++ cmd/merge_test.go
@@ -1,32 +1,38 @@
 package cmd_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/cupcicm/opp/cmd"
+	"github.com/cupcicm/opp/core"
 	"github.com/cupcicm/opp/core/tests"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMergeability(t *testing.T) {
-	a := []int{1, 2, 3}
-	b := append(a, 4)
-	b[1] = 0
-	fmt.Printf("%v", a)
-	fmt.Printf("%v", b)
+func TestCannotMergeIfDependentPRs(t *testing.T) {
 	r := tests.NewTestRepo(t)
 
-	r.CreatePr(t, "HEAD^^^", 2)
-	r.CreatePr(t, "HEAD^^", 3)
-	r.CreatePr(t, "HEAD^", 4)
-	pr5 := r.CreatePr(t, "HEAD", 5)
-	r.Repo.Checkout(pr5)
+	r.CreatePr(t, "HEAD^", 2)
+	pr3 := r.CreatePr(t, "HEAD", 3)
+	r.Repo.Checkout(pr3)
 
 	r.GithubMock.CallGetAndReturnMergeable(2, true)
-	r.GithubMock.CallGetAndReturnMergeable(3, false)
-	r.GithubMock.CallGetAndReturnMergeable(4, true)
-	r.GithubMock.CallGetAndReturnMergeable(5, true)
+	r.GithubMock.CallGetAndReturnMergeable(3, true)
 
 	assert.NotNil(t, cmd.MergeCommand(r.Repo, r.GithubMock).Execute())
+}
+
+func TestMerges(t *testing.T) {
+	r := tests.NewTestRepo(t)
+
+	pr2 := r.CreatePr(t, "HEAD", 2)
+	r.Repo.Checkout(pr2)
+
+	r.GithubMock.CallGetAndReturnMergeable(2, true)
+	r.GithubMock.CallMerge(2)
+
+	assert.Nil(t, cmd.MergeCommand(r.Repo, r.GithubMock).Execute())
+
+	// Assert that we have cleaned the local PR
+	assert.Empty(t, core.Must(r.AllLocalPrs()))
 }
