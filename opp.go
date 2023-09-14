b83d3f4b55daa745fd7c4c10701fd23b9c9434d7
Manu Cupcic <cupcicm@gmail.com>
2023-09-14 13:35:11 +0200

    Trap Ctrl C and cancel the context.

--- opp.go
+++ opp.go
@@ -4,14 +4,16 @@ import (
 	"context"
 	"log"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/cupcicm/opp/cmd"
 	"github.com/cupcicm/opp/core"
 	"github.com/spf13/viper"
 )
 
-func CommandContext() context.Context {
-	return context.Background()
+func CommandContext() (context.Context, context.CancelFunc) {
+	return context.WithTimeout(context.Background(), time.Second*12)
 }
 
 func gh(ctx context.Context) core.GhPullRequest {
@@ -27,13 +29,28 @@ func main() {
 	viper.ReadInConfig()
 
 	root := cmd.MakeApp(os.Stdout, repo, gh)
-	ctx := CommandContext()
+	ctx, cancel := CommandContext()
 	if !repo.OppEnabled() {
 		if err := root.RunContext(ctx, []string{"init"}); err != nil {
 			log.Fatal(err)
 		}
 	}
-
+	signalChan := make(chan os.Signal, 1)
+	// Get a signal when the User Ctrl-C.
+	signal.Notify(signalChan, os.Interrupt)
+	defer func() {
+		signal.Stop(signalChan)
+		cancel()
+	}()
+	go func() {
+		select {
+		case <-signalChan: // first signal, cancel context
+			cancel()
+		case <-ctx.Done():
+		}
+		<-signalChan // second signal, hard exit
+		os.Exit(2)
+	}()
 	if err := root.RunContext(ctx, os.Args); err != nil {
 		log.Fatal(err)
 	}
