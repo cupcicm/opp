f15e9643bde7410a35bec8d3e3d329cb4d0cd025
Manu Cupcic <cupcicm@gmail.com>
2023-10-20 10:02:28 +0200

    Makes opp pull more clever.
    It now tries to guess where the ancestor finishes and where the PR
    starts, and rebases only the PR commits.
    
    Notably, this should make opp work with dependent PRs with the squash
    and merge strategy (but I haven't tried :P)

--- cmd/rebase.go
+++ cmd/rebase.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"slices"
 
 	"github.com/cupcicm/opp/core"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/urfave/cli/v2"
 )
 
@@ -59,15 +61,26 @@ func rebase(ctx context.Context, repo *core.Repo, pr *core.LocalPr, first bool) 
 			fmt.Errorf(".opp/state/pr/%d is invalid, not sure what to rebase on", pr.PrNumber), 1)
 	}
 
+	ancestorCommit, err := FirstAncestorCommit(repo, pr)
+	if err != nil {
+		return false, err
+	}
+
 	if ancestor.IsPr() {
-		return rebaseOnDependentPr(ctx, repo, pr, ancestor.(*core.LocalPr), first)
+		return rebaseOnDependentPr(ctx, repo, pr, ancestor.(*core.LocalPr), ancestorCommit, first)
 	} else {
-		return rebaseOnBaseBranch(ctx, repo, pr, first)
+		return rebaseOnBaseBranch(ctx, repo, pr, ancestorCommit, first)
 	}
 }
 
 // Return true when the current PR has been merged and does not actually exist anymore.
-func rebaseOnBaseBranch(ctx context.Context, repo *core.Repo, pr *core.LocalPr, first bool) (bool, error) {
+func rebaseOnBaseBranch(
+	ctx context.Context,
+	repo *core.Repo,
+	pr *core.LocalPr,
+	parent *object.Commit,
+	first bool,
+) (bool, error) {
 	if !first {
 		fmt.Printf("Rebasing dependent PR %s...\n", pr.LocalBranch())
 	}
@@ -75,8 +88,15 @@ func rebaseOnBaseBranch(ctx context.Context, repo *core.Repo, pr *core.LocalPr, 
 	if err := repo.Checkout(pr); err != nil {
 		return false, fmt.Errorf("error during checkout: %w", err)
 	}
-	if err := repo.Rebase(ctx, repo.BaseBranch()); err != nil {
-		return false, fmt.Errorf("error during rebase: %w", err)
+	base := repo.BaseBranch()
+	if !repo.TryRebaseBranchOnto(ctx, parent.Hash, base) {
+		fmt.Printf("%s cannot be cleanly rebased on top of %s.\n", pr.LocalBranch(), base.LocalName())
+		fmt.Printf("This PR depended on another PR, and you merged a version that conflicts with this PR.\n")
+		fmt.Printf("Here is an editor where you need to choose how to correctly rebase %s on top of the new %s\n", pr.LocalBranch(), base.RemoteName())
+		err := repo.InteractiveRebase(ctx, base)
+		if err != nil {
+			return false, errors.New("please finish the interactive rebase then re-run")
+		}
 	}
 	remoteBaseBranchTip := core.Must(repo.GetRemoteTip(repo.BaseBranch()))
 	localPrTip := core.Must(repo.GetLocalTip(pr))
@@ -95,14 +115,21 @@ func rebaseOnBaseBranch(ctx context.Context, repo *core.Repo, pr *core.LocalPr, 
 // if the dependant branch has been modified git doesn't know where
 // the current PR starts exactly. The user will know.
 // Return true when the current PR has been merged and does not actually exist anymore.
-func rebaseOnDependentPr(ctx context.Context, repo *core.Repo, pr *core.LocalPr, ancestor *core.LocalPr, first bool) (bool, error) {
+func rebaseOnDependentPr(
+	ctx context.Context,
+	repo *core.Repo,
+	pr *core.LocalPr,
+	ancestor *core.LocalPr,
+	parent *object.Commit,
+	first bool,
+) (bool, error) {
 	hasBeenMerged, err := rebase(ctx, repo, ancestor, false)
 	if err != nil {
 		return false, err
 	}
 	if hasBeenMerged {
 		pr.ReloadState()
-		return rebaseOnBaseBranch(ctx, repo, pr, first)
+		return rebaseOnBaseBranch(ctx, repo, pr, parent, first)
 	}
 
 	if !first {
@@ -114,7 +141,7 @@ func rebaseOnDependentPr(ctx context.Context, repo *core.Repo, pr *core.LocalPr,
 		return false, fmt.Errorf("error during checkout: %w", err)
 	}
 	// Try to rebase silently once.
-	if !repo.TryRebaseCurrentBranchSilently(ctx, ancestor) {
+	if !repo.TryRebaseBranchOnto(ctx, parent.Hash, ancestor) {
 		fmt.Printf("%s cannot be cleanly rebased on top of %s.\n", pr.LocalBranch(), ancestor.LocalBranch())
 		fmt.Printf("This usually happens when you modified (e.g. amended) some commits in %s.\n", ancestor.LocalBranch())
 		fmt.Printf("Here is an editor window where you need to pick only the commits in %s.\n", pr.LocalBranch())
@@ -126,4 +153,36 @@ func rebaseOnDependentPr(ctx context.Context, repo *core.Repo, pr *core.LocalPr,
 	}
 	pr.RememberCurrentTip()
 	return false, nil
+}
+
+// Returns the first commit in the history of pr that belongs to its ancestor, and does
+// not belong to the PR.
+func FirstAncestorCommit(repo *core.Repo, pr *core.LocalPr) (*object.Commit, error) {
+	tip := core.Must(repo.GetLocalTip(pr))
+	commits, err := repo.GetCommitsNotInBaseBranch(tip.Hash)
+	if err != nil {
+		return nil, fmt.Errorf("%s does not descend from %s", pr.LocalBranch(), repo.BaseBranch().LocalName())
+	}
+
+	ancestorKnownTips := pr.AncestorTips()
+	ancestor, err := pr.GetAncestor()
+	if err != nil {
+		return nil, err
+	}
+	remoteTip, err := repo.GetRemoteTip(ancestor)
+	if err != nil {
+		return nil, err
+	}
+	ancestorKnownTips = append(ancestorKnownTips, remoteTip.Hash.String())
+
+	slices.Reverse(ancestorKnownTips)
+	for _, commit := range commits {
+		if slices.Contains(ancestorKnownTips, commit.Hash.String()) {
+			// The current commit was once the tip of the ancestor branch for this PR.
+			// This means that the current PR contains all commits after this, and
+			// the commits before this were the commits of the ancestor branch or PR.
+			return commit, nil
+		}
+	}
+	return remoteTip, nil
 }
