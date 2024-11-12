1ed6eb8b267ce64adfe433f67dcac0a9293364fc
cp <04philip@gmail.com>
2024-11-12 21:30:35 +0100

    Add retries to the Linear graphql client

--- core/story/linear.go
+++ core/story/linear.go
@@ -5,13 +5,30 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/avast/retry-go"
 	"github.com/machinebox/graphql"
 )
 
-const linearGraphqlEndpoint = "https://api.linear.app/graphql"
+const (
+	linearGraphqlEndpoint = "https://api.linear.app/graphql"
+	maxRetries            = 3
+)
+
+type graphqlRetryClient struct {
+	*graphql.Client
+}
+
+func (c *graphqlRetryClient) Run(ctx context.Context, req *graphql.Request, resp interface{}) error {
+	return retry.Do(
+		func() error {
+			return c.Client.Run(ctx, req, resp)
+		},
+		retry.Attempts(maxRetries),
+	)
+}
 
 type linearStoryFetcher struct {
-	client *graphql.Client
+	client *graphqlRetryClient
 }
 
 type AuthHeader struct{ Token string }
@@ -21,10 +38,11 @@ func (h AuthHeader) RoundTrip(req *http.Request) (*http.Response, error) {
 	return http.DefaultTransport.RoundTrip(req)
 }
 
-func newClient(token string) *graphql.Client {
+func newClient(token string) *graphqlRetryClient {
 	opt := graphql.WithHTTPClient(&http.Client{Transport: AuthHeader{Token: token}})
-
-	return graphql.NewClient(linearGraphqlEndpoint, opt)
+	return &graphqlRetryClient{
+		Client: graphql.NewClient(linearGraphqlEndpoint, opt),
+	}
 }
 
 func NewLinearStoryFetcher(token string) StoryFetcher {
@@ -69,7 +87,6 @@ func (l *linearStoryFetcher) FetchInProgressStories(ctx context.Context) (storie
 	// run it and capture the response
 	var respData ResponseData
 	if err := l.client.Run(ctx, req, &respData); err != nil {
-		// TODO(ClairePhi): add retry with avast retry-go
 		return nil, fmt.Errorf("failed to fetch data from the linear graphql API: %w", err)
 	}
 	for _, issue := range respData.Issues.Nodes {
