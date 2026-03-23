799aaaf53237f0e2109fd538f6b5633b2cb998ad
Manu C. <mcupcic@confluent.io>
2026-03-23 16:23:20 -0400

    adds magic value

--- core/config.go
+++ core/config.go
@@ -7,6 +7,9 @@ import (
 	"github.com/spf13/viper"
 )
 
+// TODO(manu): Use this value somewhere.
+const MagicValue = 42
+
 func init() {
 	viper.SetDefault("github.merge.method", "rebase")
 	viper.SetDefault("github.timeout", 30*time.Second)
